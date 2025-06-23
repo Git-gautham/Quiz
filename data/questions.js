b023feff75e14e422288ae3da1a81bc725ddb1e5
Git-gautham <145211278+Git-gautham@users.noreply.github.com>
2025-06-23 22:45:37 +0530

    Create questions.js

---
+++ data/questions.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "London"],
+    correct: "Paris",
+  },
+  {
+    question: "What is 2 + 2?",
+    options: ["3", "4", "5", "22"],
+    correct: "4",
+  },
+  {
+    question: "Who wrote 'Macbeth'?",
+    options: ["Shakespeare", "Keats", "Dickens", "Orwell"],
+    correct: "Shakespeare",
+  }
+];
+
+export default questions;
